646d571e6f9691bcdd95c589099f1445b6e658e5
Ashokkrishna <ashokcareer99@gmail.com>
2025-07-06 23:39:07 +0530

    Second Commit

--- _pages/about.md
+++ _pages/about.md
@@ -1,6 +1,7 @@
 ---
 permalink: /about/
 title: "About"
+classes: wide
 ---
 
 linuxsecurity.in is the personal blog of Ashok Krishna, a cybersecurity professional with a deep passion for Digital Forensics and Incident Response (DFIR), Threat Intelligence, Threat Hunting, and Adversary Simulation. This platform serves as a dedicated space to share Ashok's original research, technical deep-dives, custom tools, automation scripts, and innovative solutions crafted from real-world security challenges. Whether it’s developing niche detection techniques, exploring Linux internals, or building security automation workflows, everything shared here is rooted in practical experience and a drive to contribute to the infosec community.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/post-notice/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/welcome-to-jekyll/",
+        "teaser": null
+      }]
